32935a6c0d32e4249db0766be9dbd8291c30beca
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2025-10-20 23:49:36 +0200

    originalvibe

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Uid\Uuid;
 
 class UserController extends AbstractController
 {
